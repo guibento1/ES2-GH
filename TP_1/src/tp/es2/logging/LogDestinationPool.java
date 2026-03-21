40f744a8a1aa538a8588830cec3669ce82e2c8b1
vapa2002 <vasco.antonio.aparicio@gmail.com>
2026-03-21 20:04:52 +0000

    Desenvolvimento da classe LogDestinationPool

--- TP_1/src/tp/es2/logging/LogDestinationPool.java
+++ TP_1/src/tp/es2/logging/LogDestinationPool.java
@@ -1,4 +1,28 @@
 package tp.es2.logging;
 
 public class LogDestinationPool {
+
+    private List<LogDestination> available = new ArrayList<>(); // disponíveis
+    private List<LogDestination> inUse     = new ArrayList<>(); // em uso
+
+    public LogDestinationPool(List<LogDestination> initial) {
+        available.addAll(initial);
+    }
+
+    public LogDestination acquire() {
+        if (available.isEmpty()) {
+            throw new RuntimeException("Sem destinos disponiveis no pool");
+        }
+        LogDestination destination = available.remove(0);
+        inUse.add(destination);
+        return destination;
+    }
+
+    public void release(LogDestination destination) {
+        inUse.remove(destination);
+        available.add(destination); // devolve ao pool para reutilização
+    }
+
+    public int availableCount() { return available.size(); }
+    public int inUseCount()     { return inUse.size(); }
 }
